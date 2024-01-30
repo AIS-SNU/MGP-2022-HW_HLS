5cc44059460d321c3c4a00bdf62c9546bdc889f7
DOGEUNNKIM <kdg6245@snu.ac.kr>
2024-01-30 03:58:41 -0500

    make_HW3_HLS

---
+++ HW3_HLS/matmul/src/kernel_cpp/real.h
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+// Course:      ECE8893 - Parallel Programming for FPGAs
+// Filename:    real.h
+// Description: Header file for real matrix multiplication
+//
+// Note:        DO NOT MODIFY THIS CODE!
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __REAL_H__
+#define __REAL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <ap_int.h>
+
+typedef ap_int<16> real_t;
+
+void krnl_matmul(
+                int** a,
+                int** b,
+                int** c,
+                const int n_elements
+		);
+
+#endif
