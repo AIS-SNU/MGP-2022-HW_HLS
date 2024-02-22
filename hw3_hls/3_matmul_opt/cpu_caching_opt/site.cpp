7ea2505b9faac0e5a25da43f562b21ef9395d30e
DOGEUNNKIM <kdg6245@snu.ac.kr>
2024-02-21 23:28:01 -0500

    cpu openmp implement

--- hw3_hls/3_matmul_opt/cpu_caching_opt/site.cpp
+++ hw3_hls/3_matmul_opt/cpu_caching_opt/site.cpp
@@ -5,18 +5,19 @@
 #include <omp.h>
 #include <vector>
 
-#define MATRIX_LEN 1024*4
+#define MATRIX_LEN 2048*8
 #define DATA_SIZE MATRIX_LEN * MATRIX_LEN
 #define rows MATRIX_LEN
 #define columns MATRIX_LEN
 #define inners MATRIX_LEN
-#define tileSize 400
+#define tileSize 46
+#define tileSize_2d 300
 
 using namespace std;
 
 void matmulImplNaive(const int *left, const int *right,
                             int *result) {
-#pragma omp parallel for schedule(dynamic) shared(result, left, right) default(none)  collapse(1)  num_threads(48)
+#pragma omp parallel for schedule(dynamic) shared(result, left, right) default(none)  collapse(1)  num_threads(24)
   for (int row = 0; row < rows; row++) {
     for (int col = 0; col < columns; col++) {
       for (int inner = 0; inner < inners; inner++) {
@@ -26,7 +27,7 @@ void matmulImplNaive(const int *left, const int *right,
 
 void matmulImplLoopOrder(const int *left, const int *right,
                             int *result) {
-#pragma omp parallel num_threads(48) shared(result, left, right) default(none)
+#pragma omp parallel num_threads(24) shared(result, left, right) default(none)
 #pragma omp for nowait schedule(dynamic) collapse(1)
 
   for (int row = 0; row < rows; row++) {
@@ -38,7 +39,7 @@ void matmulImplLoopOrder(const int *left, const int *right,
 
 void matmulImplTiling(const int *left, const int *right,
                             int *result){
-#pragma omp parallel num_threads(48) shared(result, left, right) default(none) 
+#pragma omp parallel num_threads(24) shared(result, left, right) default(none) 
 #pragma omp for nowait schedule(dynamic) collapse(2)
   for (int innerTile = 0; innerTile < inners; innerTile += tileSize) {
     for (int row = 0; row < rows; row++) {
@@ -49,17 +50,32 @@ void matmulImplTiling(const int *left, const int *right,
               left[row * inners + inner] * right[inner * columns + column];
 } } } } }
 
-
+/*
 void matmulImplRowColParallelInnerTiling(const int*left,const int *right,int *result) {
 #pragma omp parallel num_threads(48) shared(result, left, right) default(none)
 #pragma omp for nowait schedule(dynamic) collapse(2)
-  for (int rowTile = 0; rowTile < rows; rowTile += 512) {
-    for (int columnTile = 0; columnTile < columns; columnTile += 512) {
-      for (int innerTile = 0; innerTile < inners; innerTile += tileSize) {
-	     for (int row = rowTile; row < rowTile + 512; row++) {
-          int innerTileEnd = std::min(inners, innerTile + tileSize);
+  for (int rowTile = 0; rowTile < rows; rowTile += 256) {
+    for (int columnTile = 0; columnTile < columns; columnTile += 256) {
+      for (int innerTile = 0; innerTile < inners; innerTile += tileSize_2d) {
+	     for (int row = rowTile; row < rowTile + 256; row++) {
+          int innerTileEnd = std::min(inners, innerTile + tileSize_2d);
           for (int inner = innerTile; inner < innerTileEnd; inner++) {
-            for (int col = columnTile; col < columnTile + 512; col++) {
+            for (int col = columnTile; col < columnTile + 256; col++) {
+              result[row * columns + col] += left[row * inners + inner] * right[inner * columns + col];
+
+} } } } } } }
+
+*/
+
+void matmulImplRowColParallelInnerTiling(const int*left,const int *right,int *result) {
+  for (int rowTile = 0; rowTile < rows; rowTile += MATRIX_LEN) {
+    for (int columnTile = 0; columnTile < columns; columnTile += 256) {
+      for (int innerTile = 0; innerTile < inners; innerTile += tileSize_2d) {
+	    #pragma omp parallel for num_threads(24) shared(columnTile,innerTile,rowTile,result, left, right) default(none) 
+		for (int row = rowTile; row < rowTile + MATRIX_LEN; row++) {
+          int innerTileEnd = std::min(inners, innerTile + tileSize_2d);
+          for (int inner = innerTile; inner < innerTileEnd; inner++) {
+            for (int col = columnTile; col < columnTile + 256; col++) {
               result[row * columns + col] += left[row * inners + inner] * right[inner * columns + col];
 
 } } } } } } }
@@ -86,7 +102,8 @@ int main(int argc, char **argv) {
 	bufReference_tile[i]=0;
 	bufReference_2dtile[i]=0;
   }
-cout<<omp_get_max_threads()<<endl;
+//cout<<omp_get_max_threads()<<endl;
+
 //un pot
 std::cout << "CPU start" << std::endl;
   auto cpu_begin = std::chrono::high_resolution_clock::now();
@@ -94,6 +111,12 @@ std::cout << "CPU start" << std::endl;
   auto cpu_end = std::chrono::high_resolution_clock::now();
 std::cout << "CPU end" << std::endl;
 
+//2d tiling
+std::cout << "CPU 2d tile mutithread  loop start" << std::endl;
+auto cpu_2dtile_begin = std::chrono::high_resolution_clock::now();
+matmulImplRowColParallelInnerTiling(&bo0[0],&bo1[0],&bufReference_2dtile[0]);
+auto cpu_2dtile_end = std::chrono::high_resolution_clock::now();
+std::cout << "CPU 2d tile mutithread loop end" << std::endl;
 
 
 //tiling
@@ -103,19 +126,13 @@ matmulImplTiling(&bo0[0],&bo1[0],&bufReference_tile[0]);
 auto cpu_tile_end = std::chrono::high_resolution_clock::now();
 std::cout << "CPU tile loop end" << std::endl;
 
+
 //reorder loop
 std::cout << "CPU reorder loop start" << std::endl;
   auto cpu_reorder_begin = std::chrono::high_resolution_clock::now();
   matmulImplLoopOrder(&bo0[0],&bo1[0],&bufReference_reorder[0]);
   auto cpu_reorder_end = std::chrono::high_resolution_clock::now();
 std::cout << "CPU reorder loop end" << std::endl;
-
-//2d tiling
-std::cout << "CPU 2d tile mutithread  loop start" << std::endl;
-auto cpu_2dtile_begin = std::chrono::high_resolution_clock::now();
-matmulImplRowColParallelInnerTiling(&bo0[0],&bo1[0],&bufReference_2dtile[0]);
-auto cpu_2dtile_end = std::chrono::high_resolution_clock::now();
-std::cout << "CPU 2d tile mutithread loop end" << std::endl;
 
 
 std::cout << "MATRIX size "<< MATRIX_LEN <<" x "<< MATRIX_LEN <<  " time results" << std::endl;
@@ -132,14 +149,14 @@ std::cout << "CPU tile Time:                       " << cpu_tile_duration.count(
 std::chrono::duration<double> cpu_2dtile_duration = cpu_2dtile_end - cpu_2dtile_begin;
 std::cout << "CPU 2d tile multithread Time:        " << cpu_2dtile_duration.count() << " s" << std::endl;
 
-//for(int i =DATA_SIZE-40 ; i<DATA_SIZE;i++){
-//std::cout <<"answer "<< bufReference[i] << " result  " << bufReference_tile[i] << std::endl;
+//for(int i =0 ; i<100;i++){
+//std::cout <<"answer "<< bufReference[i] << " result  " << bufReference_2dtile[i] << std::endl;
 //}
 
 
 if (std::memcmp( &bufReference[0], &bufReference_reorder[0], DATA_SIZE))
     throw std::runtime_error(" bufReference_reorder does not match reference");
-//if (std::memcmp( &bufReference[0], &bufReference_tile[0], DATA_SIZE))
+//if (std::memcmp( &bufReference[0], &bufReference_tile[0], DATA_SIZE-1))
 //    throw std::runtime_error("bufReference_tile does not match reference");
 if (std::memcmp( &bufReference[0], &bufReference_2dtile[0], DATA_SIZE))
     throw std::runtime_error("bufReference_2dtile does not match reference");
